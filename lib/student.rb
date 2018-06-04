2cae1aba8d152fc727e675f5e2ce03932eb2a67f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-04 01:05:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    binding.pry
-    #self.new(row[1], row[2], row[0])
+    student = self.new(row[1], row[2], row[0])
+    
   end
 
   def self.find_by_name(name)
